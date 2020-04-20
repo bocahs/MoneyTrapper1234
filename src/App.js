d075af95d1e0c2223a54ebd60f3e5fe5f3e12fd6
Jacob <bocahs@users.noreply.github.com>
2020-04-20 03:50:41 -0400

    anyfin

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Gambling from './Gambling';
 import Cashback from './Cashback';
 import Wallet from './Wallet';
 import Footer from './Footer';
-import Fade from 'react-reveal/Fade';
+import All from './All';
 
 const App = () => {
   return (
@@ -15,17 +15,12 @@ const App = () => {
       <Router>
         <Website />
         <Switch>
-          <div id='referrals'>
-            <Route exact path='/'>
-              <Route component={Earn} />
-              <Route component={Gambling} />
-              <Route component={Cashback} />
-              <Route component={Wallet} />
-            </Route>
-            <Route exact path='/earn' component={Earn} />
-            <Route exact path='/gambling' component={Gambling} />
-            <Route exact path='/cashback' component={Cashback} />
-            <Route exact path='/wallet' component={Wallet} />
+          <div id="referrals">
+            <Route exact path="/" component={All} />
+            <Route exact path="/earn" component={Earn} />
+            <Route exact path="/gambling" component={Gambling} />
+            <Route exact path="/cashback" component={Cashback} />
+            <Route exact path="/wallet" component={Wallet} />
           </div>
         </Switch>
       </Router>
